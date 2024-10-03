ba0658196ebce94c73dbcbbc080cbdc1422e62d6
RUBEN M <minaidroborubendarley@gmail.com>
2024-10-03 08:42:03 -0500

    while

---
+++ logicaWhile.js
@@ -0,0 +1,9 @@
+var numeroentrada =0;
+var resultado =0;
+var control =0
+numeroentrada = parseInt(prompt("ingrese el numero que desea calcular"));
+while (numeroentrada>=control) {
+    resultado = resultado + control;
+    control = control+1;
+}
+alert("la suma de los primeros " + numeroentrada + " natural es igual a " + resultado);
